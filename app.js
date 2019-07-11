6b511d50ef38753022907ce0c99d241b7dbb41a9
LilyLouisane <LilyLouisane@users.noreply.github.com>
2019-07-11 21:06:58 +0700

    Updated app.js, packages, added admin site

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 var express = require("express");
 var ejs = require("ejs");
 var path = require("path");
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
 
 
 var app = express();
+
+// Module requires
+
+var schemas = require('./back_ends/schemas.js');
 
 /////////////////////////
 // SETTING UP SECTION //
@@ -32,17 +38,34 @@ app.get('/', function(req, res) {
 
 app.get('/about-us', function(req, res) {
     res.render('about_us');
-})
+});
 
 app.get('/products', function(req, res) {
     res.render('products');
-})
+});
 app.get('/our-partners', function(req, res) {
     res.render('our_partners');
-})
+});
 app.get('/news', function(req, res) {
     res.render('news');
-})
+});
+
+app.get('/admin', function(req, res) {
+    res.render('admin_sign_in');
+});
+////////////////////
+// Handle Errors //
+//////////////////
+
+app.get('/err=:err_name', function(req, res) {
+    res.render('error_handler', { message: req.params.err_name })
+});
+
+
+// Handle unknown queries
+app.get('/*', function(req, res) {
+    res.redirect('/err=unknown-site');
+});
 
 
 ////////////////////////////

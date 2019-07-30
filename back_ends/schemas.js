db404a690cf1d72ddd4ab011d47e3d4154a7669e
RFedererno1 <49802669+RFedererno1@users.noreply.github.com>
2019-07-31 00:04:09 +0700

    update schemas

--- back_ends/schemas.js
+++ back_ends/schemas.js
@@ -11,6 +11,28 @@ var partner_schema = new mongoose.Schema({
     image_path: String
 });
 
+var CategorySchema = new mongoose.Schema({
+    NameCategory: String,
+    ProductCountCategory: [Number]
+});
+
+var ProductSchema = new mongoose.Schema({
+    NameProduct: String,
+    OriginProduct: String,
+    BarcodeProduct: String,
+    DescriptionProduct: String,
+    ImagePathProduct: String,
+    CategoryIdProduct: Number
+})
+
 partner_schema.plugin(autoIncrement.plugin, 'partner');
 
+CategorySchema.plugin(autoIncrement.plugin, 'category');
+
+ProductSchema.plugin(autoIncrement.plugin, 'product');
+
 module.exports.partner_schema = partner_schema;
+
+module.exports.CategorySchema = CategorySchema;
+
+module.exports.ProductSchema = ProductSchema;
